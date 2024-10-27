4b8cb378f0d8681ce295dd094622317a1014d0da
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-10-27 15:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ function Person(name, age) {
 	this.name=name;
 	this.age=age;
 }
-// Person.prototype.greet = function() {
-// 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-// }
 Person.prototype.greet = function() {
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
@@ -17,11 +14,8 @@ function Employee(name, age, jobTitle) {
 
 Employee.prototype = Object.create(Person.prototype);
 
-// Employee.prototype.jobGreet = function() {
-// 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-// }
 Employee.prototype.jobGreet = function() {
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 // Do not change code below this line
